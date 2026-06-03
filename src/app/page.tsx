0bc00b6abd1393f88118690725d49a12b3cd7d95
mustufaahmed1994-lab <mustufa.ahmed1994@gmail.com>
2026-06-03 20:15:37 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Hero from '@/components/sections/Hero'
-import ImpactBar from '@/components/sections/ImpactBar'
-import WorkIndex from '@/components/sections/WorkIndex'
-import CapabilitiesGrid from '@/components/sections/CapabilitiesGrid'
-import ContactFooter from '@/components/sections/ContactFooter'
+import { ImpactBar } from '@/components/sections/ImpactBar'
+import { WorkIndex } from '@/components/sections/WorkIndex'
+import { CapabilitiesGrid } from '@/components/sections/CapabilitiesGrid'
+import { ContactFooter } from '@/components/sections/ContactFooter'
 import Nav from '@/components/ui/Nav'
 
 export default function Home() {
