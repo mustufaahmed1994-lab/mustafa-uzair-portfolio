3d1782dc2ae6f0770198fde8d86629029e6ab7ab
mustufaahmed1994-lab <mustufa.ahmed1994@gmail.com>
2026-06-03 20:16:23 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { ImpactBar } from '@/components/sections/ImpactBar'
 import { WorkIndex } from '@/components/sections/WorkIndex'
 import { CapabilitiesGrid } from '@/components/sections/CapabilitiesGrid'
 import { ContactFooter } from '@/components/sections/ContactFooter'
-import Nav from '@/components/ui/Nav'
+import { Nav } from '@/components/ui/Nav'
 
 export default function Home() {
   return (
