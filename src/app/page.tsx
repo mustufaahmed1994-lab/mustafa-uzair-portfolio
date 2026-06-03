78cef631045ceab55d197ccb8c46eefbc1ceb242
mustufaahmed1994-lab <mustufa.ahmed1994@gmail.com>
2026-06-03 20:07:40 +0500

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import Hero from '@/components/sections/Hero'
+import ImpactBar from '@/components/sections/ImpactBar'
+import WorkIndex from '@/components/sections/WorkIndex'
+import CapabilitiesGrid from '@/components/sections/CapabilitiesGrid'
+import ContactFooter from '@/components/sections/ContactFooter'
+import Nav from '@/components/ui/Nav'
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <main>
+        <Hero />
+        <ImpactBar />
+        <WorkIndex />
+        <CapabilitiesGrid />
+        <ContactFooter />
+      </main>
+    </>
+  )
+}
